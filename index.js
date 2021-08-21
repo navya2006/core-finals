8ee8f21753e42dd76ba6c897a7650007de72a8fc
navya2006 <navya2006.github.com>
2021-08-21 08:41:52 +0530

    thik kar rahi hu

--- index.js
+++ index.js
@@ -17,13 +17,25 @@ jarallaxVideo();
       }
   }); 
   
-    // SIDENAV 
-	function openNav(){
-		document.getElementById("sidenav").style.width="100%";
-	}
-	function closeNav () {
-		document.getElementById("sidenav").style.width="0";
-	}
+      // disappearing navbar
+      jQuery(document).on("scroll", function () {
+        if ($(document).scrollTop() > 200) {
+            $(".navb").css("background-color","rgba(0,0,0,0.6)");
+            $(".disappear").css("top","0");
+        } else {
+            $(".navb").css("background","none");
+            $(".disappear").css("top","-200");
+        }
+    }); 
+    
+      // SIDENAV 
+      function openNav(){
+          document.getElementById("sidenav").style.width="100%";
+      }
+      function closeNav () {
+          document.getElementById("sidenav").style.width="0";
+      }
+      
 	
 	//ajib
     function auto_height(elem) {
